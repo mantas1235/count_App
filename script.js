573a403e957a4947d5c6b490edb0d739a4210ba4
Mantas Santockis <mantas.san@gmail.com>
2023-04-14 22:37:27 +0300

    finish program

--- script.js
+++ script.js
@@ -6,14 +6,53 @@ const count = document.querySelector(".count")
 const add = document.querySelector(".add")
 const resetCount = document.querySelector(".reset")
 const sub = document.querySelector(".subtract")
+const buttons = document.querySelector(".buttons")
 
 
-add.addEventListener("click", function () {
-    count.innerHTML++
+
+buttons.addEventListener("click", function (e) {
+    if (e.target.classList.contains("add")) {
+        count.innerHTML++
+        setColor()
+    }
+    else if (e.target.classList.contains("subtract")) {
+        count.innerHTML--
+        setColor()
+    }
+    else if (e.target.classList.contains("reset")) {
+        count.innerHTML = 0
+        setColor()
+    }
+
 })
-sub.addEventListener("click", function () {
-    count.innerHTML--
-})
-resetCount.addEventListener("click", function () {
-    count.innerHTML = 0
-})
+
+const setColor = () => {
+    if (count.innerHTML > 0) {
+        count.style.color = "yellow"
+    }
+
+    else if (count.innerHTML < 0) {
+        count.style.color = "red"
+    }
+
+    else {
+        count.style.color = "#fff"
+    }
+
+
+
+}
+
+
+
+
+
+// add.addEventListener("click", function () {
+//     count.innerHTML++
+// })
+// sub.addEventListener("click", function () {
+//     count.innerHTML--
+// })
+// resetCount.addEventListener("click", function () {
+//     count.innerHTML = 0
+// })
