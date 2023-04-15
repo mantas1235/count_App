a24de952728877b96db8ca91072e5b6bb0aaa025
Mantas Santockis <mantas.san@gmail.com>
2023-04-15 09:33:17 +0300

    create css and html

--- script.js
+++ script.js
@@ -2,57 +2,23 @@
 
 
 
-const count = document.querySelector(".count")
-const add = document.querySelector(".add")
-const resetCount = document.querySelector(".reset")
-const sub = document.querySelector(".subtract")
-const buttons = document.querySelector(".buttons")
 
 
 
-buttons.addEventListener("click", function (e) {
-    if (e.target.classList.contains("add")) {
-        count.innerHTML++
-        setColor()
-    }
-    else if (e.target.classList.contains("subtract")) {
-        count.innerHTML--
-        setColor()
-    }
-    else if (e.target.classList.contains("reset")) {
-        count.innerHTML = 0
-        setColor()
-    }
 
-})
+const generateBtn = document.querySelector(".generate")
 
-const setColor = () => {
-    if (count.innerHTML > 0) {
-        count.style.color = "yellow"
-    }
-
-    else if (count.innerHTML < 0) {
-        count.style.color = "red"
-    }
-
-    else {
-        count.style.color = "#fff"
-    }
+const number = document.querySelector(".number")
 
 
+const generateNumber = () => {
+    //generate number btw 1 and 100
+
+    const randomNumber = Math.floor(Math.random() * 100 + 1)
+
+    number.innerHTML = randomNumber
 
 }
 
+generateBtn.addEventListener("click", generateNumber)
 
-
-
-
-// add.addEventListener("click", function () {
-//     count.innerHTML++
-// })
-// sub.addEventListener("click", function () {
-//     count.innerHTML--
-// })
-// resetCount.addEventListener("click", function () {
-//     count.innerHTML = 0
-// })
